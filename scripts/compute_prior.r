fb7f80535af38f90df380c7a97474487e7573202
Seong-Hwan Jun <s2jun.uw@gmail.com>
2014-03-06 02:03:23 -0800

    no message

--- scripts/compute_prior.r
+++ scripts/compute_prior.r
@@ -6,6 +6,7 @@ seeds<-read.csv("data/tourney_seeds.csv")
 # calculate the prior of being right if choosing higher seeded team
 N<-dim(tourney)[1]
 upset<-0
+upsets<-rep(0, 16)
 for (i in 1:N)
 {
   seasonID<-tourney[i,"season"]
@@ -18,6 +19,11 @@ for (i in 1:N)
   seedw<-as.numeric(substr(wseed[,2], 2, 3))
   seedl<-as.numeric(substr(lseed[,2], 2, 3))
   
+  if ((seedw + seedl) == 17)
+  {
+    upsets[seedw] <- upsets[seedw] + 1
+  }
+  
   upset <- upset + as.numeric(seedw < seedl)
   if (is.na(upset))
   {
@@ -26,5 +32,6 @@ for (i in 1:N)
   }
 }
 
-upset/N # shows that choosing higher seed does not work
+upset/N # shows that simply choosing higher seed does not work
+upsets # first round results -- #1 seed has never lost to #16 seed
 
