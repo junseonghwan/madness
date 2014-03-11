0fc29dbb36148af21c93599444ac0d76ea57fbb8
Seong-Hwan Jun <s2jun.uw@gmail.com>
2014-03-10 18:09:21 -0700

    some papers + compute tournament stats

--- scripts/compute_prior.r
+++ scripts/compute_prior.r
@@ -5,33 +5,57 @@ seeds<-read.csv("data/tourney_seeds.csv")
 
 # calculate the prior of being right if choosing higher seeded team
 N<-dim(tourney)[1]
-upset<-0
-upsets<-rep(0, 16)
+upset<-0 # total upsets
+total<-0 # total matches (two different seeds)
+results1<-rep(0, 16) # results of round 1
+upset2<-0 # upsets beyond round 1
+total2<-0
 for (i in 1:N)
 {
   seasonID<-tourney[i,"season"]
   wt<-tourney[i,"wteam"]
   lt<-tourney[i,"lteam"]
 
-  wseed<-subset(seeds, season==seasonID & team==wt)
-  lseed<-subset(seeds, season==seasonID & team==lt)
+  wseed<-as.character(subset(seeds, season==seasonID & team==wt)[,2])
+  lseed<-as.character(subset(seeds, season==seasonID & team==lt)[,2])
+
+  seedw<-as.numeric(substr(wseed, 2, nchar(wseed)))
+  seedl<-as.numeric(substr(lseed, 2, nchar(lseed)))
+
+  if (is.na(seedw))
+  {
+    # this is due to there being a character at the end (a or b for 16 seeded team)
+    seedw<-as.numeric(substr(wseed, 2, nchar(wseed)-1))
+  }
+  if (is.na(seedl))
+  {
+    # this is due to there being a character at the end (A or B for 16 seeded team)
+    seedl<-as.numeric(substr(lseed, 2, nchar(lseed)-1))
+  }
   
-  seedw<-as.numeric(substr(wseed[,2], 2, 3))
-  seedl<-as.numeric(substr(lseed[,2], 2, 3))
+  
+  if (seedw == seedl)
+  {
+    #print(paste(seedw, seedl, sep=" vs "))
+    next
+  }
   
   if ((seedw + seedl) == 17)
   {
-    upsets[seedw] <- upsets[seedw] + 1
+    results1[seedw] <- upsets[seedw] + 1
+  }
+  else
+  {
+    upset2 <- upset2 + as.numeric(seedw < seedl)
+    total2 <- total2 + 1
   }
   
   upset <- upset + as.numeric(seedw < seedl)
-  if (is.na(upset))
-  {
-    print(paste(i, upset, sep=","))
-    break
-  }
+  total <- total + 1
 }
 
-upset/N # shows that simply choosing higher seed does not work
-upsets # first round results -- #1 seed has never lost to #16 seed
-
+upset/total # shows that simply choosing higher seed does not work
+results1  # first round results -- #1 seed has never lost to #16 seed
+sum(results1[1:8])/sum(results1)
+sum(results1[9:16]) # upsets in the first round
+upset2/total2
--- scripts/explore.r
+++ scripts/explore.r
@@ -1,7 +1,9 @@
 library(RSQLite)
+library(plyr)
 
 # don't forget to setwd()
- setwd("madness/")
+setwd("madness/")
+rm(list=ls())
 
 sqlite<-dbDriver("SQLite")
 ncaaDB1<-dbConnect(sqlite, "data/ncaa.db")
@@ -9,5 +11,26 @@ dbListTables(ncaaDB1)
 
 games<-dbGetQuery(ncaaDB1, "select * from game_data_2012")
 players<-dbGetQuery(ncaaDB1, "select * from player_data_2012")
-names(gameData2012)
-head(gameData2012)
+
+games<-subset(games, home_team_id != 0 & away_team_id != 0)
+
+home<-ddply(games, c("home_team_id"), function(d) {
+  c(sum(d$home_team_pts > d$away_team_pts), mean(d$home_team_fga), sd(d$home_team_fga), dim(d)[1])
+})
+
+home2<-subset(home, V4 > 1)
+
+away<-ddply(games, c("away_team_id"), function(d) {
+  c(sum(d$home_team_pts > d$away_team_pts), mean(d$home_team_fga), sd(d$home_team_fga), dim(d)[1])
+})
+
+away2<-subset(away, V4 > 1)
+
+dukeH<-subset(games, home_team_name == "Duke")
+dukeA<-subset(games, away_team_name == "Duke")
+
+mean(dukeH$home_team_fga)
+sd(dukeH$home_team_fga)
+mean(dukeA$away_team_fga)
+sd(dukeA$away_team_fga)
+
